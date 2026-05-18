4c03739feb1482f4a6b13c59c7ce87ebaa69d9a8
RazSoft <razgroupgame@gmail.com>
2026-05-18 23:53:45 +0530

    Set the react router v7 with the basic auth routes and component intialization.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import HomePage from "./pages/HomePage";
 import "./App.css";
+import Register from "./pages/auth/Register";
+import ForgetPassword from "./pages/auth/ForgetPassword";
+import Login from "./pages/auth/Login";
+import AuthLayout from "./pages/auth/AuthLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: HomePage,
+  },
+  {
+    path: "auth",
+    Component: AuthLayout,
+    children: [
+      { path: "login", Component: Login },
+      { path: "register", Component: Register },
+      { path: "forget-password", Component: ForgetPassword },
+    ],
+  },
+]);
 
 function App() {
   return (
     <>
+      <RouterProvider router={router} />
       <div className="px-3 border-2 border-amber-300">
         Karay Suchi setup with the tailwind
       </div>
---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,3 @@
+export default function Layout() {
+  return <div className="w-100 h-dvh bg-amber-300"></div>;
+}
