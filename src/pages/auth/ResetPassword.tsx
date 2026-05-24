b75f04382c34a445e517ffbd79ec64d2d790fba4
RazSoft <razgroupgame@gmail.com>
2026-05-24 21:55:46 +0530

    STYLE (auth): Add UI element for the forget password and reset password.

--- src/pages/auth/ResetPassword.tsx
+++ src/pages/auth/ResetPassword.tsx
@@ -1,17 +1,18 @@
-export default function ForgetPassword() {
+export default function ResetPassword() {
   return (
     <div className="w-[320px] border border-slate-500 shadow-lg  rounded-xl px-4 py-4">
       <div className="w-full">
         <p className="w-full text-center text-2xl font-semibold">
-          Forget Password
+          Reset Password
         </p>
       </div>
       <div className="flex flex-col gap-2">
         <div className="w-full flex flex-col gap-1">
-          <label>Enter your email ID to find your account</label>
+          <label>Enter New Password: </label>
+          <input className="border rounded-md px-2 py-1" type="email" />
         </div>
         <div className="w-full flex flex-col gap-1">
-          <label>Email ID:</label>
+          <label>Confirm New Password: </label>
           <input className="border rounded-md px-2 py-1" type="email" />
         </div>
         <div className="flex justify-center items-center w-full mt-5 mb-4">
@@ -19,7 +20,7 @@ export default function ForgetPassword() {
             className="border-2 cursor-pointer hover:bg-black hover:text-white hover:duration-300 px-4 py-2 rounded-lg"
             type="submit"
           >
-            Find Account
+            Set New Password
           </button>
         </div>
       </div>
