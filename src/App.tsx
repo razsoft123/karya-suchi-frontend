3bcdb54df790a1126c04681e70cff5e8a2883f35
RazSoft <razgroupgame@gmail.com>
2026-05-24 23:50:15 +0530

    STYLE: Add some UI element and Layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import HomePage from "./pages/HomePage";
 import "./App.css";
 import Register from "./pages/auth/Register";
 import ForgetPassword from "./pages/auth/ForgetPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
 import Login from "./pages/auth/Login";
 import AuthLayout from "./pages/auth/AuthLayout";
+import DashboardLayout from "./components/layout/DashboardLayout";
+import Dashboard from "./pages/dashboard/Dashboard";
+import ProtectedLayouts from "./components/layout/ProtectedLayout";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +23,17 @@ const router = createBrowserRouter([
       { path: "login", Component: Login },
       { path: "register", Component: Register },
       { path: "forget-password", Component: ForgetPassword },
+      { path: "reset-password", Component: ResetPassword },
+    ],
+  },
+  {
+    Component: ProtectedLayouts,
+    children: [
+      {
+        path: "dashboard",
+        Component: DashboardLayout,
+        children: [{ index: true, Component: Dashboard }],
+      },
     ],
   },
 ]);
