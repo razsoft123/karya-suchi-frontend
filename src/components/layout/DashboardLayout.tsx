11ba8b3047e859d27a43cd02578a1a2d2f391916
RazSoft <razgroupgame@gmail.com>
2026-05-25 22:31:54 +0530

    FEAT: Add react-hot-toast for toast notifications in Dashboard and Auth layouts

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router";
+import { Toaster } from "react-hot-toast";
 export default function DashboardLayout() {
   return (
     <>
+      <Toaster position="top-right" reverseOrder={false} />
       <Outlet />
     </>
   );
--- src/pages/auth/AuthLayout.tsx
+++ src/pages/auth/AuthLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import { Toaster } from "react-hot-toast";
 
 export default function AuthLayout() {
   return (
     <div className="w-full min-h-dvh flex justify-center items-center">
+      <Toaster position="top-right" reverseOrder={false} />
       <Outlet />
     </div>
   );
