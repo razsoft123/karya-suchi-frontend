38f67a676ac5ee784bd4762d11b56a177895ca79
RazSoft <razgroupgame@gmail.com>
2026-06-03 08:55:48 +0530

    FEAT: Update Dashboard layout with greeting and quick action sections; clean up Topbar component

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -1,10 +1,5 @@
 export default function Topbar() {
   return (
-    <nav className="w-full flex justify-between items-center px-8 py-2 border-b-2">
-      <div>Karaya Suchi</div>
-      <div>
-        <div className="w-8 h-8 rounded-full bg-app-bg border-2"></div>
-      </div>
-    </nav>
+    <nav className="w-full flex justify-between items-center px-8 py-2"></nav>
   );
 }
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,84 @@
+import { NavLink } from "react-router";
+import { fakeUser } from "../../fakeData";
+import { fakeAllTasks } from "../../fakeData";
+
 export default function Dashboard() {
-  return <main>This is dashboard</main>;
+  return (
+    <main className="px-4 py-4 border-2">
+      <div className="flex flex-col gap-6">
+        {/* Greeting section */}
+        {/* Good Morning, Rohit 👋 You have 6 tasks due today, 2 overdue tasks, 3 upcoming deadlines */}
+        <section>
+          <h1 className="font-inter text-3xl font-bold">
+            Good Morning, {fakeUser.name}
+          </h1>
+          <p className="font-inter text-body text-slate-600">
+            You have 6 tasks due today, 2 overdue tasks, 3 upcoming deadlines
+          </p>
+        </section>
+
+        {/* Quick actions section */}
+        {/* + New Task, + New Note, + New Workspace, + Start Whiteboard */}
+        <section className="flex gap-4">
+          <QuickActionCard text="New Task" to="/workspace/task/new" />
+          <QuickActionCard text="New Note" to="/workspace/note/new" />
+          <QuickActionCard text="New Workspace" to="/workspace/new" />
+        </section>
+
+        {/* Today's task section */}
+        {/* Today's Tasks ✓ Design Landing Page, ✓ Fix Authentication Bug, ○ Prepare Interview, ○ Deploy Backend */}
+        <section>
+          <div>{/* Task cards */}</div>
+        </section>
+
+        {/* Recent Notes */}
+        {/* Recent Notes, Project Architecture, Interview Preparation, Karaya Suchi Roadmap */}
+        <section>
+          <div>{/* Notes cards */}</div>
+        </section>
+
+        {/* Upcoming deadline */}
+        {/* Upcoming "Tomorrow Frontend Review",  "Jun 10 Project Submission", "Jun 15 Release Planning" */}
+
+        {/* Workspace activity */}
+        {/* Workspaces Personal 12 Tasks Karaya Suchi 34 Tasks Interview Prep 7 Tasks */}
+
+        {/* Productivity summary */}
+        {/* Completed Today: 8, Pending: 12, Overdue: 2, Completion Rate: 84% */}
+      </div>
+    </main>
+  );
+}
+
+interface QuickActionCardType {
+  text: string;
+  to: string;
+}
+
+function QuickActionCard({ text, to }: QuickActionCardType) {
+  return (
+    <NavLink to={to}>
+      <div className="border border-slate-300 px-6 py-4 rounded-2xl">
+        <div className="flex gap-2">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          </div>
+          <span className="font-inter text-lg">{text}</span>
+        </div>
+      </div>
+    </NavLink>
+  );
 }
