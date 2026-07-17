85678d9eff83a44e91b9192616a84a6587845df4
RazSoft <razgroupgame@gmail.com>
2026-07-17 22:54:04 +0530

    FEAT (STYLE): Add hover in nav item

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router";
+import navbarStyles from "./navbar.module.css";
 
 export default function Navbar() {
   const [theme, setTheme] = useState("light");
@@ -17,16 +18,18 @@ export default function Navbar() {
 
   return (
     <nav className="font-inter w-full flex  justify-between px-4 py-2">
-      <div className="text-lg font-bold">Karaya Suchi</div>
+      <div className="text-lg font-bold">
+        <NavLink to="/">Karaya Suchi</NavLink>
+      </div>
       <div className="flex justify-center items-center gap-2">
         <ul className="flex gap-2 text-base font-semibold">
-          <li>
+          <li className={`${navbarStyles.navItem}`}>
             <NavLink to="/feature">Feature</NavLink>
           </li>
-          <li>
+          <li className={`${navbarStyles.navItem}`}>
             <NavLink to="/help">Help</NavLink>
           </li>
-          <li>
+          <li className="px-4 font-semibold text-base border-2 rounded-lg cursor-pointer bg-app-bg text-app-body hover:text-white hover:bg-black">
             <NavLink to="/auth/login">Get Started</NavLink>
           </li>
         </ul>
