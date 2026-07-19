eb0d262f4cb50e170db0ecf24117cec36b9967b9
RazSoft <razgroupgame@gmail.com>
2026-07-19 23:32:34 +0530

    FEAT (STYLE): Update Navbar styles and improve layout for better user experience:

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -17,28 +17,28 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="font-inter w-full flex  justify-between px-4 py-2">
-      <div className="text-lg font-bold">
+    <nav className="font-inter w-full flex  justify-between px-6 py-4 border-b-2 border-b-app-border">
+      <div className="text-2xl text-h1 text-heading-text font-bold">
         <NavLink to="/">Karaya Suchi</NavLink>
       </div>
-      <div className="flex justify-center items-center gap-2">
-        <ul className="flex gap-2 text-base font-semibold">
+      <div className="flex justify-center items-center gap-8">
+        <ul className="flex justify-center items-center gap-8 text-base font-semibold">
           <li className={`${navbarStyles.navItem}`}>
-            <NavLink to="/feature">Feature</NavLink>
+            <NavLink to="/feature">Features</NavLink>
           </li>
           <li className={`${navbarStyles.navItem}`}>
             <NavLink to="/help">Help</NavLink>
           </li>
-          <li className="px-4 font-semibold text-base border-2 rounded-lg cursor-pointer bg-app-bg text-app-body hover:text-white hover:bg-black">
+          <li className="px-4 py-2 font-semibold bg-black text-white border-2 rounded-lg cursor-pointer hover:text-black hover:bg-white">
             <NavLink to="/auth/login">Get Started</NavLink>
           </li>
+          <button
+            onClick={() => toggleTheme()}
+            className="px-4 py-2 text-body-text text-base border rounded-lg cursor-pointer bg-app-bg text-app-body hover:text-white hover:bg-black"
+          >
+            Dark Mode
+          </button>
         </ul>
-        <button
-          onClick={() => toggleTheme()}
-          className="px-4 font-semibold text-base border-2 rounded-lg cursor-pointer bg-app-bg text-app-body"
-        >
-          Dark Mode
-        </button>
       </div>
     </nav>
   );
