78ead7c6617110532ca0d894438fa386362dce11
RazSoft <razgroupgame@gmail.com>
2026-07-19 23:38:05 +0530

    FEAT (STYLE): Enhance Footer styles and correct capitalization in copyright notice

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,44 +1,44 @@
 export default function Footer() {
   return (
-    <footer className="font-inter flex flex-col gap-4 px-4 py-2 w-full">
+    <footer className="font-inter flex flex-col gap-4 px-4 py-2 w-full border-t-2 border-app-border bg-black text-white">
       <div className="flex justify-between">
         <div>
-          <div>Karay Suchi</div>
+          {/* <div>Karay Suchi</div>
           <p></p>
-          <div>Social Icons</div>
+          <div>Social Icons</div> */}
         </div>
 
         <div>
-          <div>product</div>
+          {/* <div>product</div>
           <div>feature</div>
           <div>pricing</div>
           <div>Roadmap</div>
           <div>Changes Log</div>
-          <div>API Docs</div>
+          <div>API Docs</div> */}
         </div>
 
         <div>
-          <div>Resources</div>
+          {/* <div>Resources</div>
           <div>Blog</div>
           <div>Help Center</div>
           <div>Tutorials</div>
           <div>Community</div>
-          <div>Documentation</div>
+          <div>Documentation</div> */}
         </div>
 
         <div>
-          <div>Company</div>
+          {/* <div>Company</div>
           <div>About</div>
           <div>Contact</div>
           <div>Privicy Policy</div>
           <div>Terms and Condition</div>
-          <div>Cookie Policy</div>
+          <div>Cookie Policy</div> */}
         </div>
       </div>
 
       <div className="flex flex-col justify-center items-center">
         <p>2026 Copyrighted to razsoft all rights reserved</p>
-        <p>From the soil of bihar ❤️</p>
+        <p>From the soil of Bihar ❤️</p>
       </div>
     </footer>
   );
